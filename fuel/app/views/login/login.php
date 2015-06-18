a6ec131b2b924d81b3dea8417352ca9c2b3df556
terry6969 <terry@fox-hound.jp>
2015-06-18 15:57:51 +0900

    TOPお手本

--- fuel/app/views/login/login.php
+++ fuel/app/views/login/login.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
 <!-- ログイン用View -->
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Loginページ</title>
-</head>
-<body>
+<?php echo View::forge('inc/header'); ?>
 
-<?php
-	echo Asset::js('shop.js'); 
-?>
-
-	<form action="/login/login" method="POST" onsubmit="confirm_login()">
-		<input type="text" name="id" id="id">:ID<br>
-		<input type="text" name="pass" id="pass">:PASS<br>
-		<input type="submit" value="ログイン">
-<?php 
-	if (isset($msg)) {
-		print "$msg";
-	}
-?>
-	</form>
-</body>
-</html>
+<div class="row" style="margin-top:200px;">
+	<div class="container" style="width:200px;">
+		<form action="/login/login" method="POST" data-toggle="validator">
+			<?php if(isset($msg)): ?>
+			<div class="row">
+				<span class="glyphicon glyphicon-ok text-danger" aria-hidden="true"></span><span class="text-danger" ><?php echo $msg; ?></span>
+			</div>
+			<?php endif; ?>
+			<div class="form-group">
+    			<label for="input_id">ID</label>
+				<input type="text" class="form-control" name="id" id="input_id"  maxlength="20" required>
+			</div>
+			<div class="form-group">
+    			<label for="input_pass">PASSWORD</label>
+				<input type="password" class="form-control" name="pass" id="input_pass" pattern="^[A-Za-z0-9]+$" maxlength="20" required>
+			</div>
+			<input class="btn btn-success" type="submit" value="ログイン" >
+		<form>
+	</div>
+</div>
+	
+<?php echo View::forge('inc/footer'); ?>
